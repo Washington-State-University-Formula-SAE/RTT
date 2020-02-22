3851dfc3df41fd5dcb864a32bc3d985c83f9592d
Thomas Swanzey <swanzem11@gmail.com>
2020-02-22 14:42:56 -0800

    String split testing

--- Pi/receive.py
+++ Pi/receive.py
@@ -1,4 +1,3 @@
-import MQTTDevice
 import os
 import can
 
@@ -9,10 +8,10 @@ os.system('sudo ifconfig can0 up')
 can0 = can.interface.Bus(channel = 'can0', bustype = 'socketcan_ctypes')
 
 # IOT
-sas = "SharedAccessSignature sr=hackathon2020fsae.azure-devices.net&sig=u7v565eWC1MMstYzDcl5kCXepcluJh8zcFko7SKTieg%3D&skn=iothubowner&se=1582764444"
-iot_hub_name = "hackathon2020fsae"
-device_id = "EngineRPM"
-device = MQTTDevice.MQTTDevice(sas,iot_hub_name, device_id )
+# sas = "SharedAccessSignature sr=hackathon2020fsae.azure-devices.net&sig=u7v565eWC1MMstYzDcl5kCXepcluJh8zcFko7SKTieg%3D&skn=iothubowner&se=1582764444"
+# iot_hub_name = "hackathon2020fsae"
+# device_id = "EngineRPM"
+# device = MQTTDevice.MQTTDevice(sas,iot_hub_name, device_id )
 
 # Receive messages
 while True:
