51e1d7edb0afd01bfd63a0993d1739d2b550254c
Adam Possing <atom@possing.com>
2020-02-22 13:50:14 -0800

    telemetry service working

--- RealTimeTelemetry/RTTWeb/Data/TelemetryService.cs
+++ RealTimeTelemetry/RTTWeb/Data/TelemetryService.cs
@@ -1,12 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Host;
 using Microsoft.Azure.WebJobs.ServiceBus;
+using Microsoft.Azure.EventHubs;
 
 using Models;
+using System.Text;
 
 namespace RTTWeb.Data
 {
@@ -15,14 +18,91 @@ namespace RTTWeb.Data
         // https://stackoverflow.com/questions/45889855/simple-way-to-get-data-from-iot-hub-in-c-sharp
         // https://docs.microsoft.com/en-us/azure/iot-hub/quickstart-send-telemetry-dotnet
 
+        // Event Hub-compatible endpoint
+        // az iot hub show --query properties.eventHubEndpoints.events.endpoint --name {your IoT Hub name}
+        private readonly static string s_eventHubsCompatibleEndpoint = "sb://ihsuprodbyres069dednamespace.servicebus.windows.net/";
+
+        // Event Hub-compatible name
+        // az iot hub show --query properties.eventHubEndpoints.events.path --name {your IoT Hub name}
+        private readonly static string s_eventHubsCompatiblePath = "iothub-ehub-hackathon2-2973704-9523dece74";
+
+        // az iot hub policy show --name service --query primaryKey --hub-name {your IoT Hub name}
+        private readonly static string s_iotHubSasKey = "hzFFMn2gKpYbF7DO6gYJxHaOmkS2h+UK4ffIrkIsjJk=";
+        private readonly static string s_iotHubSasKeyName = "service";
+        private static EventHubClient s_eventHubClient;
         TelemetryModel tm = new TelemetryModel();
-    }
-    public static class Function1
-    {
-        [FunctionName("Function1")]
-        public static void Run([EventHubTrigger("myEventHubName", Connection = "myIoTHub")]string myEventHubMessage, TraceWriter log)
+        public TelemetryService()
         {
-            log.Info($"C# Event Hub trigger function processed a message: {myEventHubMessage}");
+            var connectionString = new EventHubsConnectionStringBuilder(new Uri(s_eventHubsCompatibleEndpoint), s_eventHubsCompatiblePath, s_iotHubSasKeyName, s_iotHubSasKey);
+            s_eventHubClient = EventHubClient.CreateFromConnectionString(connectionString.ToString());
+
+            // Create a PartitionReciever for each partition on the hub.
+            var runtimeInfo = s_eventHubClient.GetRuntimeInformationAsync().Result;
+            var d2cPartitions = runtimeInfo.PartitionIds;
+
+            CancellationTokenSource cts = new CancellationTokenSource();
+
+            Console.CancelKeyPress += (s, e) =>
+            {
+                e.Cancel = true;
+                cts.Cancel();
+                Console.WriteLine("Exiting...");
+            };
+
+
+            var tasks = new List<Task>();
+            foreach (string partition in d2cPartitions)
+            {
+                tasks.Add(ReceiveMessagesFromDeviceAsync(partition, cts.Token));
+            }
+
+        }
+
+
+        // Asynchronously create a PartitionReceiver for a partition and then start 
+        // reading any messages sent from the simulated client.
+        private static async Task ReceiveMessagesFromDeviceAsync(string partition, CancellationToken ct)
+        {
+            // Create the receiver using the default consumer group.
+            // For the purposes of this sample, read only messages sent since 
+            // the time the receiver is created. Typically, you don't want to skip any messages.
+            var eventHubReceiver = s_eventHubClient.CreateReceiver("$Default", partition, EventPosition.FromEnqueuedTime(DateTime.Now));
+            Console.WriteLine("Create receiver on partition: " + partition);
+            while (true)
+            {
+                if (ct.IsCancellationRequested) break;
+                Console.WriteLine("Listening for messages on: " + partition);
+                // Check for EventData - this methods times out if there is nothing to retrieve.
+                var events = await eventHubReceiver.ReceiveAsync(100);
+
+                // If there is data in the batch, process it.
+                if (events == null) continue;
+
+                foreach (EventData eventData in events)
+                {
+
+
+                    string data = Encoding.UTF8.GetString(eventData.Body.Array);
+                    Console.WriteLine("Message received on partition {0}:", partition);
+                    Console.WriteLine("  {0}:", data);
+                    Console.WriteLine("Application properties (set by device):");
+                    foreach (var prop in eventData.Properties)
+                    {
+                        Console.WriteLine("  {0}: {1}", prop.Key, prop.Value);
+                    }
+                    Console.WriteLine("System properties (set by IoT Hub):");
+                    foreach (var prop in eventData.SystemProperties)
+                    {
+                        Console.WriteLine("  {0}: {1}", prop.Key, prop.Value);
+                    }
+                }
+            }
         }
     }
+    class ReadDeviceToCloudMessages
+    {
+        
+    }
 }
+
+
