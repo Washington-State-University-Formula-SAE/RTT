6e20b972592b9e037c438471e4b07ada821c5a0e
Thomas Swanzey <swanzem11@gmail.com>
2020-02-22 14:32:28 -0800

    Split spring test

--- Pi/receive.py
+++ Pi/receive.py
@@ -1,34 +1,23 @@
-##############################################################################
-
-## Description :  This codes is for test two USB2CAN module commuincation
-##                one as sender and the other as receiver.
-##                sender send  '0,1,2,3,4,5,6'
- 
-## Author      :  Calvin (calvin@inno-maker.com)/ www.inno-maker.com
-              
-                
-## Date        :  2019.11.30
-
-## Environment :  Hardware            ----------------------  Raspberry Pi 4
-##                SYstem of RPI       ----------------------  2019-09-26-raspbian-buster-full.img
-##                Version of Python   ----------------------  Python 3.7.3(Default in the system)
-## Toinstall dependencies:
-## sudo pip install python-can
-
-
-###############################################################################
-
-
+import MQTTDevice
 import os
 import can
 
+
+# CAN
 os.system('sudo ip link set can0 type can bitrate 500000')
 os.system('sudo ifconfig can0 up')
-
 can0 = can.interface.Bus(channel = 'can0', bustype = 'socketcan_ctypes')
 
+# IOT
+sas = "SharedAccessSignature sr=hackathon2020fsae.azure-devices.net&sig=u7v565eWC1MMstYzDcl5kCXepcluJh8zcFko7SKTieg%3D&skn=iothubowner&se=1582764444"
+iot_hub_name = "hackathon2020fsae"
+device_id = "EngineRPM"
+device = MQTTDevice.MQTTDevice(sas,iot_hub_name, device_id )
+
+# Receive messages
 while True:
     msg = can0.recv(30.0)
+    msg.split()
     print (msg)
     if msg is None:
         print('No message was received')
