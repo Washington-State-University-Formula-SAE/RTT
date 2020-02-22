11d6c1a7761f2647d1fa0cb9a5d4e9ceb252e407
Austin Baccus <gunrak117@live.com>
2020-02-22 12:39:31 -0800

    Removing old stuff.

--- RealTimeTelemetry/RTTWeb/Data/TelemetryService.cs
+++ RealTimeTelemetry/RTTWeb/Data/TelemetryService.cs
@@ -2,10 +2,27 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Host;
+using Microsoft.Azure.WebJobs.ServiceBus;
+
+using Models;
 
 namespace RTTWeb.Data
 {
     public class TelemetryService
     {
+        // https://stackoverflow.com/questions/45889855/simple-way-to-get-data-from-iot-hub-in-c-sharp
+        // https://docs.microsoft.com/en-us/azure/iot-hub/quickstart-send-telemetry-dotnet
+
+        TelemetryModel tm = new TelemetryModel();
+    }
+    public static class Function1
+    {
+        [FunctionName("Function1")]
+        public static void Run([EventHubTrigger("myEventHubName", Connection = "myIoTHub")]string myEventHubMessage, TraceWriter log)
+        {
+            log.Info($"C# Event Hub trigger function processed a message: {myEventHubMessage}");
+        }
     }
 }
