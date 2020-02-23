bd8a7b831f25f38a2ca076c868f13c4cda6b0872
Ben <swanzeyb@students.svsd410.org>
2020-02-23 00:19:50 -0800

    Added common vehicle stat models

---
+++ RealTimeTelemetry/Models/WheelSpeed.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Models
+{
+    public class WheelSpeed
+    {
+        public DateTime TimeStamp { get; set; }
+        public int Speed { get; set; }
+    }
+}
