976a2a994d03567a3746aee24642986085e277ea
Poonslayer69420 <austinbaccus@live.com>
2020-02-22 17:00:21 -0800

    Implemented ChartJs.Blazor visual example.

--- RealTimeTelemetry/Models/TelemetryModel.cs
+++ RealTimeTelemetry/Models/TelemetryModel.cs
@@ -3,5 +3,6 @@
     public class TelemetryModel
     {
         public double EngineRpm { get; set; }
+        public string EngineRpmStr { get; set; }
     }
 }
--- RealTimeTelemetry/RTTWeb/Startup.cs
+++ RealTimeTelemetry/RTTWeb/Startup.cs
@@ -9,8 +9,6 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using RTTWeb.Data;
-
 namespace RTTWeb
 {
     public class Startup
@@ -28,7 +26,7 @@ namespace RTTWeb
         {
             services.AddRazorPages();
             services.AddServerSideBlazor();
-            services.AddSingleton<WeatherForecastService>();
+            services.AddSingleton<Data.WeatherForecastService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
