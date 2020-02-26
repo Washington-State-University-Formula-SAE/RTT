db0d61522b99e017ddeab47a6b6756fbb5e8d2f3
Thomas Swanzey <swanzem11@gmail.com>
2020-02-26 12:16:49 -0800

    Implemented switch statement for CAN ids, changed code hierarchy, cleaned up code and added additional modularity

--- Pi/main.py
+++ Pi/main.py
@@ -2,9 +2,12 @@ from paho.mqtt import client as mqtt
 import time
 import ssl
 import MQTT.MQTTDevice
-from can_receive import CANInterface
+from CANDataInterpreter import CANInterface
 
 if __name__ == "__main__":
     can = CANInterface()
-    can.start_receive_serial()
+    com = "COM3"
+    baud = 9600
+
+    can.start_receive_serial(com, baud)
     
